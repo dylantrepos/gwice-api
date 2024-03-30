d8bb4b28c6ae6b0028609f3c548aa485615de17c
Dylan Trepos <dylan.trepos@gmail.com>
2024-03-30 20:54:51 +0100

    feat(database): init sequelize

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,11 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     OPEN_AGENDA_API_KEY: string;
+    DATABASE_URL: string;
+    PGHOST: string;
+    PGDATABASE: string;
+    PGUSER: string;
+    PGPASSWORD: string;
+    ENDPOINT_ID: string;
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import "dotenv/config.js";
-import Router from "./routes/routes";
-import express from 'express';
-import cors from 'cors';
 import bodyParser from "body-parser";
-import http from 'http';
-// import { checkConnection } from "./mongo/connection";
-import { cron } from "./cron/cron";
+import cors from "cors";
+import { config } from "dotenv";
+import "dotenv/config.js";
+import express from "express";
+import http from "http";
+import { DataTypes, Sequelize } from "sequelize";
+import Router from "./routes/routes";
 
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || "localhost";
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,11 +17,52 @@ app.use(Router);
 
 const server = http.createServer(app);
 
-const connectMongo = async () => {  
-  // await cron();
-}
+/**
+ ** Postgres Sequelize connection
+ */
 
-connectMongo();
+config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: { ssl: { require: true } },
+});
+
+// Set up the models
+const Author = require("../models/author")(sequelize, DataTypes);
+const Book = require("../models/book")(sequelize, DataTypes);
+
+// Create a new Express application
+app.get("/", async (_, res) => {
+  res.send("Hello World! This is a book catalog.");
+});
+
+app.get("/authors", async (_, res) => {
+  try {
+    const authors = await Author.findAll();
+    res.json(authors);
+  } catch (error) {
+    console.error("Error fetching authors:", error);
+    res.status(500).send("Error fetching authors");
+  }
+});
+
+app.get("/books/:author_id", async (req, res) => {
+  const authorId = parseInt(req.params.author_id);
+  try {
+    const books = await Book.findAll({
+      where: {
+        authorId: authorId,
+      },
+    });
+    res.json(books);
+  } catch (error) {
+    console.error("Error fetching books for author:", error);
+    res.status(500).send("Error fetching books for author");
+  }
+});
 
 server.listen(+PORT, HOST, () => {
   console.clear();
